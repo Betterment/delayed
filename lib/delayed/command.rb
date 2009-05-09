17ddfb53f597e45bb59bd54376791437b452fcbc
Patrick McKenzie <patrick@bingocardcreator.com>
2009-05-09 13:51:33 +0900

    Added ability to run multiple workers concurrently.

--- lib/delayed/command.rb
+++ lib/delayed/command.rb
@@ -4,8 +4,11 @@ require 'optparse'
 
 module Delayed
   class Command
+    attr_accessor :worker_count
+    
     def initialize(args)
       @options = {:quiet => true}
+      @worker_count = 1
       
       opts = OptionParser.new do |opts|
         opts.banner = "Usage: #{File.basename($0)} [options] start|stop|restart|run"
@@ -23,17 +26,23 @@ module Delayed
         opts.on('--max-priority N', 'Maximum priority of jobs to run.') do |n|
           @options[:max_priority] = n
         end
+        opts.on('-n', '--number_of_workers=workers', "Number of unique workers to spawn") do |worker_count|
+          @worker_count = worker_count.to_i rescue 1
+        end
       end
       @args = opts.parse!(args)
     end
   
     def daemonize
-      Daemons.run_proc('delayed_job', :dir => "#{RAILS_ROOT}/tmp/pids", :dir_mode => :normal, :ARGV => @args) do |*args|
-        run
+      worker_count.times do |worker_index|
+        process_name = worker_count == 1 ? "delayed_job" : "delayed_job.#{worker_index}"
+        Daemons.run_proc(process_name, :dir => "#{RAILS_ROOT}/tmp/pids", :dir_mode => :normal, :ARGV => @args) do |*args|
+          run process_name
+        end
       end
     end
     
-    def run
+    def run(worker_name = nil)
       require File.join(RAILS_ROOT, 'config', 'environment')
       
       # Replace the default logger
@@ -42,6 +51,7 @@ module Delayed
       ActiveRecord::Base.logger = logger
       ActiveRecord::Base.clear_active_connections!
       Delayed::Worker.logger = logger
+      Delayed::Job.worker_name = "#{worker_name} #{Delayed::Job.worker_name}"
       
       Delayed::Worker.new(@options).start  
     rescue => e
