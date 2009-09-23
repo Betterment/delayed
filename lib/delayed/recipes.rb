6c12ab7062e1507bd082d82d9980ce4bfc81b202
Brandon Keepers <brandon@collectiveidea.com>
2009-09-22 20:07:18 -0400

    Set RAILS_ENV in cap task since the --environment option is currently broken.

--- lib/delayed/recipes.rb
+++ lib/delayed/recipes.rb
@@ -11,17 +11,17 @@ Capistrano::Configuration.instance.load do
   namespace :delayed_job do
     desc "Stop the delayed_job process"
     task :stop, :roles => :app do
-      run "cd #{current_path}; script/delayed_job -e #{rails_env} stop"
+      run "cd #{current_path}; RAILS_ENV=#{rails_env} script/delayed_job stop"
     end
 
     desc "Start the delayed_job process"
     task :start, :roles => :app do
-      run "cd #{current_path}; script/delayed_job -e #{rails_env} start"
+      run "cd #{current_path}; RAILS_ENV=#{rails_env} script/delayed_job start"
     end
 
     desc "Restart the delayed_job process"
     task :restart, :roles => :app do
-      run "cd #{current_path}; script/delayed_job -e #{rails_env} restart"
+      run "cd #{current_path}; RAILS_ENV=#{rails_env} script/delayed_job restart"
     end
   end
 end
