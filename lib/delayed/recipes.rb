491871edb5f2a6646846cb7248841022e007d3f4
Brandon Keepers <brandon@collectiveidea.com>
2009-11-27 11:35:39 -0600

    Don't use rails_env in capistrano if it's not set. closes #22

--- lib/delayed/recipes.rb
+++ lib/delayed/recipes.rb
@@ -9,19 +9,23 @@
 
 Capistrano::Configuration.instance.load do
   namespace :delayed_job do
+    def rails_env
+      fetch(:rails_env, false) ? "RAILS_ENV=#{fetch(:rails_env)}" : ''
+    end
+    
     desc "Stop the delayed_job process"
     task :stop, :roles => :app do
-      run "cd #{current_path}; RAILS_ENV=#{rails_env} script/delayed_job stop"
+      run "cd #{current_path};#{rails_env} script/delayed_job stop"
     end
 
     desc "Start the delayed_job process"
     task :start, :roles => :app do
-      run "cd #{current_path}; RAILS_ENV=#{rails_env} script/delayed_job start"
+      run "cd #{current_path};#{rails_env} script/delayed_job start"
     end
 
     desc "Restart the delayed_job process"
     task :restart, :roles => :app do
-      run "cd #{current_path}; RAILS_ENV=#{rails_env} script/delayed_job restart"
+      run "cd #{current_path};#{rails_env} script/delayed_job restart"
     end
   end
 end
