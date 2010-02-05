4d46b5fbd7b25104563b97a2e436e52ee79865b9
Brandon Keepers <brandon@collectiveidea.com>
2010-02-04 22:27:18 -0500

    Set auto_flushing to true on logger to disable Rails' buffered logging

--- lib/delayed/command.rb
+++ lib/delayed/command.rb
@@ -61,6 +61,7 @@ module Delayed
       end
       
       Delayed::Worker.logger = Rails.logger
+      Delayed::Worker.logger.auto_flushing = true
       ActiveRecord::Base.connection.reconnect!
       
       worker = Delayed::Worker.new(@options)
