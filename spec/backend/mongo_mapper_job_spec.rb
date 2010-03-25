d848ae2f5d0738d1d4480dbd57c3c7b37bebc8b8
Fred Wu <canglan@gmail.com>
2010-03-25 17:49:25 +1100

    Syntax error on some systems due to the encoding.

--- spec/backend/mongo_mapper_job_spec.rb
+++ spec/backend/mongo_mapper_job_spec.rb
@@ -40,30 +40,30 @@ describe Delayed::Backend::MongoMapper::Job do
     end
     
     it "should ignore not found errors because they are permanent" do
-      story = MongoStory.create :text => 'Once upon a time…'
+      story = MongoStory.create :text => 'Once upon a time...'
       job = story.send_later(:tell)
       story.destroy
       lambda { job.invoke_job }.should_not raise_error
     end
 
     it "should store the object as string" do
-      story = MongoStory.create :text => 'Once upon a time…'
+      story = MongoStory.create :text => 'Once upon a time...'
       job = story.send_later(:tell)
 
       job.payload_object.class.should   == Delayed::PerformableMethod
       job.payload_object.object.should  == "LOAD;MongoStory;#{story.id}"
       job.payload_object.method.should  == :tell
       job.payload_object.args.should    == []
-      job.payload_object.perform.should == 'Once upon a time…'
+      job.payload_object.perform.should == 'Once upon a time...'
     end
 
     it "should store arguments as string" do
-      story = MongoStory.create :text => 'Once upon a time…'
+      story = MongoStory.create :text => 'Once upon a time...'
       job = MongoStoryReader.new.send_later(:read, story)
       job.payload_object.class.should   == Delayed::PerformableMethod
       job.payload_object.method.should  == :read
       job.payload_object.args.should    == ["LOAD;MongoStory;#{story.id}"]
-      job.payload_object.perform.should == 'Epilog: Once upon a time…'
+      job.payload_object.perform.should == 'Epilog: Once upon a time...'
     end
   end
 end
