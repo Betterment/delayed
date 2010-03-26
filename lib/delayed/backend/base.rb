36b558dc3a2e22f3a71c0b110980509ef32f8dc0
Brandon Keepers <brandon@collectiveidea.com>
2010-03-26 10:29:01 -0400

    Re-added Delayed::Job.work_off and deprecated it

--- lib/delayed/backend/base.rb
+++ lib/delayed/backend/base.rb
@@ -28,6 +28,11 @@ module Delayed
         # Hook method that is called after a new worker is forked
         def after_fork
         end
+        
+        def work_off(num = 100)
+          warn "[DEPRECATION] `Delayed::Job.work_off` is deprecated. Use `Delayed::Worker.new.work_off instead."
+          Delayed::Worker.new.work_off(num)
+        end
       end
       
       ParseObjectFromYaml = /\!ruby\/\w+\:([^\s]+)/
