71fbb4fb94e157d51f1f0418990244341accfa13
David Genord II <david@xspond.com>
2010-07-16 12:03:09 -0400

    Correct deprecation notice in Rails 3

--- lib/delayed/backend/active_record.rb
+++ lib/delayed/backend/active_record.rb
@@ -25,11 +25,18 @@ module Delayed
         
         before_save :set_default_run_at
 
-        named_scope :ready_to_run, lambda {|worker_name, max_run_time|
-          {:conditions => ['(run_at <= ? AND (locked_at IS NULL OR locked_at < ?) OR locked_by = ?) AND failed_at IS NULL', db_time_now, db_time_now - max_run_time, worker_name]}
-        }
-        named_scope :by_priority, :order => 'priority ASC, run_at ASC'
-        
+        if ::ActiveRecord::VERSION::MAJOR >= 3
+          scope :ready_to_run, lambda {|worker_name, max_run_time|
+            where(['(run_at <= ? AND (locked_at IS NULL OR locked_at < ?) OR locked_by = ?) AND failed_at IS NULL', db_time_now, db_time_now - max_run_time, worker_name])
+          }
+          scope :by_priority, order('priority ASC, run_at ASC')
+        else
+          named_scope :ready_to_run, lambda {|worker_name, max_run_time|
+            {:conditions => ['(run_at <= ? AND (locked_at IS NULL OR locked_at < ?) OR locked_by = ?) AND failed_at IS NULL', db_time_now, db_time_now - max_run_time, worker_name]}
+          }
+          named_scope :by_priority, :order => 'priority ASC, run_at ASC'
+        end
+
         def self.after_fork
           ::ActiveRecord::Base.connection.reconnect!
         end
