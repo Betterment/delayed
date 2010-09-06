fa5aab7b614169b491a166399a0993537c936752
Brandon Keepers <brandon@collectiveidea.com>
2010-09-06 18:49:10 -0400

    Don't require Rails and ActionMailer since delayed_job can be used in plain ol' ruby apps with Active Record

--- lib/delayed_job.rb
+++ lib/delayed_job.rb
@@ -2,11 +2,11 @@ require 'active_support'
 
 require File.dirname(__FILE__) + '/delayed/message_sending'
 require File.dirname(__FILE__) + '/delayed/performable_method'
-require File.dirname(__FILE__) + '/delayed/performable_mailer'
+require File.dirname(__FILE__) + '/delayed/performable_mailer' if defined?(ActionMailer)
 require File.dirname(__FILE__) + '/delayed/yaml_ext'
 require File.dirname(__FILE__) + '/delayed/backend/base'
 require File.dirname(__FILE__) + '/delayed/worker'
-require File.dirname(__FILE__) + '/delayed/railtie'
+require File.dirname(__FILE__) + '/delayed/railtie' if defined?(Rails::Railtie)
 
 Object.send(:include, Delayed::MessageSending)   
 Module.send(:include, Delayed::MessageSending::ClassMethods)
