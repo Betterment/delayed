626b5e21bbf92cafa765c1edebc0b1235e9554e3
Brandon Keepers <brandon@collectiveidea.com>
2010-11-12 13:11:05 -0600

    remove log_queries option.  It will be replaced with better logger settings

--- lib/delayed/worker.rb
+++ lib/delayed/worker.rb
@@ -6,13 +6,12 @@ require 'logger'
 
 module Delayed
   class Worker
-    cattr_accessor :min_priority, :max_priority, :max_attempts, :max_run_time, :default_priority, :sleep_delay, :logger, :log_queries
+    cattr_accessor :min_priority, :max_priority, :max_attempts, :max_run_time, :default_priority, :sleep_delay, :logger
     self.sleep_delay = 5
     self.max_attempts = 25
     self.max_run_time = 4.hours
     self.default_priority = 0
-    self.log_queries = true
-    
+
     # By default failed jobs are destroyed after too many attempts. If you want to keep them around
     # (perhaps to inspect the reason for the failure), set this to false.
     cattr_accessor :destroy_failed_jobs
