deb329beadf00abc646252197ce8fcdeebfee620
Matt Griffin <matt@griffinonline.org>
2010-11-29 17:13:43 -0500

    When using the activerecord backend, reserve jobs with an update query instead of trying to lock a batch of available jobs.  This causes workers to serialize on the database and avoids contention between them.

--- lib/generators/delayed_job/templates/migration.rb
+++ lib/generators/delayed_job/templates/migration.rb
@@ -13,6 +13,7 @@ class CreateDelayedJobs < ActiveRecord::Migration
     end
 	
     add_index :delayed_jobs, [:priority, :run_at], :name => 'delayed_jobs_priority'
+    add_index :delayed_jobs, :locked_by, :name => 'delayed_jobs_locked_by'
   end
   
   def self.down
