05635776a7ac220b49f4c30457afa1d7eca43497
Paul Annesley <paul@sitepoint.com>
2011-02-03 21:46:35 +1100

    Guard against attempting YAML::ENGINE.yamler = "syck" in older rubies.

--- lib/delayed/yaml_ext.rb
+++ lib/delayed/yaml_ext.rb
@@ -2,7 +2,7 @@
 # Classes, Modules and Structs
 
 require 'yaml'
-YAML::ENGINE.yamler = "syck"
+YAML::ENGINE.yamler = "syck" if defined?(YAML::ENGINE)
 
 class Module
   yaml_as "tag:ruby.yaml.org,2002:module"
