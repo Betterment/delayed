b4a80598faf9e6399e2208c26a50b5c9ffa45afd
Matt Griffin <matt@griffinonline.org>
2011-05-31 16:48:43 -0400

    Bring back AR-specific serialization since it's likely to be used even with a non-AR backend.

--- lib/delayed_job.rb
+++ lib/delayed_job.rb
@@ -8,6 +8,7 @@ require File.dirname(__FILE__) + '/delayed/backend/base'
 require File.dirname(__FILE__) + '/delayed/worker'
 require File.dirname(__FILE__) + '/delayed/deserialization_error'
 require File.dirname(__FILE__) + '/delayed/railtie' if defined?(Rails::Railtie)
+require File.dirname(__FILE__) + '/delayed/serialization/active_record' if defined?(ActiveRecord)
 
 Object.send(:include, Delayed::MessageSending)
 Module.send(:include, Delayed::MessageSending::ClassMethods)
