b30362263080425402dbf8addbc0105c6a563c2d
Matt Griffin <matt@griffinonline.org>
2011-08-30 17:48:20 -0400

    Allow Rails 2 to deserialize AR objects.

--- lib/delayed/serialization/active_record.rb
+++ lib/delayed/serialization/active_record.rb
@@ -2,7 +2,11 @@ class ActiveRecord::Base
   yaml_as "tag:ruby.yaml.org,2002:ActiveRecord"
 
   def self.yaml_new(klass, tag, val)
-    klass.unscoped.find(val['attributes'][klass.primary_key])
+    if ActiveRecord::VERSION::MAJOR == 3
+      klass.unscoped.find(val['attributes'][klass.primary_key])
+    else # Rails 2
+      klass.with_exclusive_scope { klass.find(val['attributes'][klass.primary_key]) }
+    end
   rescue ActiveRecord::RecordNotFound
     raise Delayed::DeserializationError
   end
