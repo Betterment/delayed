39aa92af3792f7c5740902ce3c215c2a5760468f
Brian Ryckbost <bryckbost@gmail.com>
2011-09-26 16:55:11 -0400

    Change class_inheritable_accessor to class_attribute in Delayed::Plugin

--- lib/delayed/plugin.rb
+++ lib/delayed/plugin.rb
@@ -1,11 +1,11 @@
 module Delayed
   class Plugin
-    class_inheritable_accessor :callback_block
+    class_attribute :callback_block
 
     def self.callbacks(&block)
       self.callback_block = block
     end
-    
+
     def initialize
       self.class.callback_block.call(Delayed::Worker.lifecycle) if self.class.callback_block
     end
