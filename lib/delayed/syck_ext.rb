79cce653c977b1ad0806dcdcecb11042f3edd7b8
Brian Ryckbost <bryckbost@gmail.com>
2011-09-28 09:46:37 -0400

    Move ActiveRecord serialization to syck_ext

--- lib/delayed/syck_ext.rb
+++ lib/delayed/syck_ext.rb
@@ -1,3 +1,21 @@
+class ActiveRecord::Base
+  yaml_as "tag:ruby.yaml.org,2002:ActiveRecord"
+
+  def self.yaml_new(klass, tag, val)
+    if ActiveRecord::VERSION::MAJOR == 3
+      klass.unscoped.find(val['attributes'][klass.primary_key])
+    else # Rails 2
+      klass.with_exclusive_scope { klass.find(val['attributes'][klass.primary_key]) }
+    end
+  rescue ActiveRecord::RecordNotFound
+    raise Delayed::DeserializationError
+  end
+
+  def to_yaml_properties
+    ['@attributes']
+  end
+end
+
 class Module
   yaml_as "tag:ruby.yaml.org,2002:module"
 
