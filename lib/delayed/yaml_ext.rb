72ba8863b755043c6b38d9c24e9506f3cdebede3
Brian Ryckbost <bryckbost@gmail.com>
2011-09-28 16:25:35 -0400

    Add back the active_record serialization file.
    It's needed for the delayed_job_actice_record backend, but is only required if the Yaml.parser is using Syck.

--- lib/delayed/yaml_ext.rb
+++ lib/delayed/yaml_ext.rb
@@ -4,6 +4,7 @@
 require 'yaml'
 if YAML.parser.class.name =~ /syck/i
   require File.expand_path('../syck_ext', __FILE__)
+  require File.expand_path('../serialization/active_record', __FILE__)
 else
   require File.expand_path('../psych_ext', __FILE__)
 end
