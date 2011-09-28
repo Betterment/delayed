a9d0f47cbe31552feea80915be630cd158b82c20
Brian Ryckbost <bryckbost@gmail.com>
2011-09-28 10:34:54 -0400

    Load syck if Ruby 1.8, otherwise we load Psych and require yaml.

--- lib/delayed/yaml_ext.rb
+++ lib/delayed/yaml_ext.rb
@@ -1,10 +1,11 @@
 # These extensions allow properly serializing and autoloading of
 # Classes, Modules and Structs
 
-require 'yaml'
-
-if YAML::ENGINE.syck?
+if RUBY_VERSION < '1.9.0'
+  require 'syck'
   require File.expand_path('../syck_ext', __FILE__)
 else
+  require 'psych'
   require File.expand_path('../psych_ext', __FILE__)
 end
+require 'yaml'
