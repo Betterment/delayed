904c2c2777b02c467c3ab1d285723b26d360d3b7
Brian Ryckbost <bryckbost@gmail.com>
2011-09-28 10:48:07 -0400

    Make the yaml ext spec descriptions more clear.

--- spec/yaml_ext_spec.rb
+++ spec/yaml_ext_spec.rb
@@ -1,21 +1,21 @@
 require 'spec_helper'
 
 describe "YAML" do
-  it "should autoload classes that are unknown at runtime" do
+  it "should autoload classes" do
     lambda {
       yaml = "--- !ruby/class:Autoloaded::Clazz {}\n"
       YAML.load(yaml).should == Autoloaded::Clazz
     }.should_not raise_error
   end
 
-  it "should autoload a struct" do
+  it "should autoload the class of a struct" do
     lambda {
       yaml = "--- !ruby/class:Autoloaded::Struct {}\n"
       YAML.load(yaml).should == Autoloaded::Struct
     }.should_not raise_error
   end
 
-  it "should autoload structs that are unknown at runtime" do
+  it "should autoload the class for the instance of a struct" do
     lambda {
       yaml = "--- !ruby/struct:Autoloaded::InstanceStruct {}"
       YAML.load(yaml).class.should == Autoloaded::InstanceStruct
