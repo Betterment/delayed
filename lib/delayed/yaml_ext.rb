df8622c196acad6464a2d777a291e5e7d2ae1cc0
Brian Ryckbost <bryckbost@gmail.com>
2011-09-28 14:33:58 -0400

    Rather than looking at the RUBY_VERSION, check the YAML.parser.

--- lib/delayed/yaml_ext.rb
+++ lib/delayed/yaml_ext.rb
@@ -1,11 +1,9 @@
 # These extensions allow properly serializing and autoloading of
 # Classes, Modules and Structs
 
-if RUBY_VERSION < '1.9.0'
-  require 'syck'
+require 'yaml'
+if YAML.parser.class.name =~ /syck/i
   require File.expand_path('../syck_ext', __FILE__)
 else
-  require 'psych'
   require File.expand_path('../psych_ext', __FILE__)
 end
-require 'yaml'
