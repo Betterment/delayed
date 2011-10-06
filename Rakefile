690e25f63a1c206dddd91c25e805772e37f0013d
Brian Ryckbost <bryckbost@gmail.com>
2011-10-06 12:19:41 -0400

    Remove ruby gems require since we're using Bundler.

--- Rakefile
+++ Rakefile
@@ -1,5 +1,4 @@
 # -*- encoding: utf-8 -*-
-require 'rubygems'
 require 'bundler/setup'
 Bundler::GemHelper.install_tasks
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,6 @@
 $:.unshift(File.dirname(__FILE__) + '/../lib')
 
-require 'rubygems'
 require 'bundler/setup'
-require 'rspec'
 require 'logger'
 
 require 'rails'
