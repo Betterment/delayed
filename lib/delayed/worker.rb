b1d5f401fb556bad92ae4e14fa2cc0128beb8f03
Brian Ryckbost <bryckbost@gmail.com>
2011-11-30 15:15:16 -0500

    Remove conditional loading of the serialization backend.
    The logic already exists in mongoid's serialization backend file

--- lib/delayed/worker.rb
+++ lib/delayed/worker.rb
@@ -37,7 +37,7 @@ module Delayed
 
     def self.backend=(backend)
       if backend.is_a? Symbol
-        require "delayed/serialization/#{backend}" if YAML.parser.class.name =~ /syck/i
+        require "delayed/serialization/#{backend}"
         require "delayed/backend/#{backend}"
         backend = "Delayed::Backend::#{backend.to_s.classify}::Job".constantize
       end
