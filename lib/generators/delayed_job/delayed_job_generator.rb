6aaa90054f056beeee0de51685b735f7a6388252
Brian Ryckbost <bryckbost@gmail.com>
2011-12-15 13:53:40 -0500

    Whitespace cleanup

--- lib/generators/delayed_job/delayed_job_generator.rb
+++ lib/generators/delayed_job/delayed_job_generator.rb
@@ -1,9 +1,9 @@
 require 'rails/generators'
 
 class DelayedJobGenerator < Rails::Generators::Base
-  
+
   self.source_paths << File.join(File.dirname(__FILE__), 'templates')
-  
+
   def create_script_file
     template 'script', 'script/delayed_job'
     chmod 'script/delayed_job', 0755
