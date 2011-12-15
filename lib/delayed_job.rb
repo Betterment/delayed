5982e993f925dbc4ea63bc4461dce0d2699cf45e
Brian Ryckbost <bryckbost@gmail.com>
2011-12-15 13:54:54 -0500

    Remove File.dirnames
    RubyGems manages the load path so requiring files is a lot cleaner than
    requiring them with File.dirname(__FILE__)

--- lib/delayed_job.rb
+++ lib/delayed_job.rb
@@ -1,22 +1,21 @@
 require 'active_support'
-
-require File.dirname(__FILE__) + '/delayed/message_sending'
-require File.dirname(__FILE__) + '/delayed/performable_method'
+require 'delayed/message_sending'
+require 'delayed/performable_method'
 
 # PerformableMailer is compatible with ActionMailer 3 (and possibly 3.1)
 if defined?(ActionMailer)
   require 'action_mailer/version'
-  require File.dirname(__FILE__) + '/delayed/performable_mailer' if 3 == ActionMailer::VERSION::MAJOR
+  require 'delayed/performable_mailer' if 3 == ActionMailer::VERSION::MAJOR
 end
 
-require File.dirname(__FILE__) + '/delayed/yaml_ext'
-require File.dirname(__FILE__) + '/delayed/lifecycle'
-require File.dirname(__FILE__) + '/delayed/plugin'
-require File.dirname(__FILE__) + '/delayed/plugins/clear_locks'
-require File.dirname(__FILE__) + '/delayed/backend/base'
-require File.dirname(__FILE__) + '/delayed/worker'
-require File.dirname(__FILE__) + '/delayed/deserialization_error'
-require File.dirname(__FILE__) + '/delayed/railtie' if defined?(Rails::Railtie)
+require 'delayed/yaml_ext'
+require 'delayed/lifecycle'
+require 'delayed/plugin'
+require 'delayed/plugins/clear_locks'
+require 'delayed/backend/base'
+require 'delayed/worker'
+require 'delayed/deserialization_error'
+require 'delayed/railtie' if defined?(Rails::Railtie)
 
 Object.send(:include, Delayed::MessageSending)
 Module.send(:include, Delayed::MessageSending::ClassMethods)
