ad98535f49de406069d755d42838ce9fc4f76bbc
David Genord II <david@collectiveidea.com>
2012-11-09 10:03:46 -0500

    Move spec for root implementation details to worker_spec from shared_spec

--- spec/worker_spec.rb
+++ spec/worker_spec.rb
@@ -16,4 +16,25 @@ describe Delayed::Worker do
       expect(Delayed::Worker.backend).to eq(Delayed::Backend::Test::Job)
     end
   end
+
+  context "worker read-ahead" do
+    before do
+      @read_ahead = Delayed::Worker.read_ahead
+    end
+
+    after do
+      Delayed::Worker.read_ahead = @read_ahead
+    end
+
+    it "reads five jobs" do
+      Delayed::Job.should_receive(:find_available).with(anything, 5, anything).and_return([])
+      Delayed::Job.reserve(Delayed::Worker.new)
+    end
+
+    it "reads a configurable number of jobs" do
+      Delayed::Worker.read_ahead = 15
+      Delayed::Job.should_receive(:find_available).with(anything, Delayed::Worker.read_ahead, anything).and_return([])
+      Delayed::Job.reserve(Delayed::Worker.new)
+    end
+  end
 end
