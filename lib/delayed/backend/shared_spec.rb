692786bc921471c0acc5083a217ac880b8a925fb
David Genord II <david@collectiveidea.com>
2014-09-24 11:49:40 -0400

    Correct use of stub

--- lib/delayed/backend/shared_spec.rb
+++ lib/delayed/backend/shared_spec.rb
@@ -427,12 +427,12 @@ shared_examples_for 'a delayed_job backend' do
     end
 
     it 'uses the max_run_time value on the payload when defined' do
-      @job.payload_object.stub(:max_run_time).and_return(30.minutes)
+      expect(@job.payload_object).to receive(:max_run_time).and_return(30.minutes)
       expect(@job.max_run_time).to eq(30.minutes)
     end
 
     it 'results in an overridden run time when defined' do
-      @job.payload_object.stub(:max_run_time).and_return(45.minutes)
+      expect(@job.payload_object).to receive(:max_run_time).and_return(45.minutes)
       expect(worker.max_run_time(@job)).to eq(45.minutes)
     end
   end
