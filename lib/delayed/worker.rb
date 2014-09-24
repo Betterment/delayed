293fa6cb53a2dc1b1aaced335b326b7b7e61805e
David Genord II <david@collectiveidea.com>
2014-09-24 10:42:34 -0400

    Logging within signal handlers is weird
    Thanks @christophermanning

--- lib/delayed/worker.rb
+++ lib/delayed/worker.rb
@@ -132,13 +132,13 @@ module Delayed
 
     def start # rubocop:disable CyclomaticComplexity, PerceivedComplexity
       trap('TERM') do
-        say 'Exiting...'
+        Thread.new { say 'Exiting...' }
         stop
         raise SignalException, 'TERM' if self.class.raise_signal_exceptions
       end
 
       trap('INT') do
-        say 'Exiting...'
+        Thread.new { say 'Exiting...' }
         stop
         raise SignalException, 'INT' if self.class.raise_signal_exceptions && self.class.raise_signal_exceptions != :term
       end
