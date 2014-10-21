6d0a6b2e0d67435640bbb9b98ed026cf7952e6b3
David Genord II <david@collectiveidea.com>
2014-10-21 14:36:55 -0400

    One of the merges duplicated a lot of specs

--- spec/performable_method_spec.rb
+++ spec/performable_method_spec.rb
@@ -58,16 +58,6 @@ describe Delayed::PerformableMethod do
       end
     end
 
-    %w[before after success].each do |hook|
-      it "delegates #{hook} hook to object" do
-        story = Story.create
-        job = story.delay.tell
-
-        expect(story).to receive(hook).with(job)
-        job.invoke_job
-      end
-    end
-
     it 'delegates enqueue hook to object' do
       story = Story.create
       expect(story).to receive(:enqueue).with(an_instance_of(Delayed::Job))
@@ -75,13 +65,6 @@ describe Delayed::PerformableMethod do
     end
 
     it 'delegates error hook to object' do
-      story = Story.create
-      expect(story).to receive(:error).with(an_instance_of(Delayed::Job), an_instance_of(RuntimeError))
-      expect(story).to receive(:tell).and_raise(RuntimeError)
-      expect { story.delay.tell.invoke_job }.to raise_error
-    end
-
-    it 'delegates error hook to object when delay_jobs = false' do
       story = Story.create
       expect(story).to receive(:error).with(an_instance_of(Delayed::Job), an_instance_of(RuntimeError))
       expect(story).to receive(:tell).and_raise(RuntimeError)
@@ -111,14 +94,6 @@ describe Delayed::PerformableMethod do
         end
       end
 
-      %w[before after success].each do |hook|
-        it "delegates #{hook} hook to object" do
-          story = Story.create
-          expect(story).to receive(hook).with(an_instance_of(Delayed::Job))
-          story.delay.tell
-        end
-      end
-
       it 'delegates error hook to object' do
         story = Story.create
         expect(story).to receive(:error).with(an_instance_of(Delayed::Job), an_instance_of(RuntimeError))
@@ -126,15 +101,7 @@ describe Delayed::PerformableMethod do
         expect { story.delay.tell }.to raise_error
       end
 
-      it 'delegates error hook to object when delay_jobs = false' do
-        story = Story.create
-        expect(story).to receive(:error).with(an_instance_of(Delayed::Job), an_instance_of(RuntimeError))
-        expect(story).to receive(:tell).and_raise(RuntimeError)
-        expect { story.delay.tell }.to raise_error
-      end
-
-      it 'delegates failure hook to object when delay_jobs = false' do
-        Delayed::Worker.delay_jobs = false
+      it 'delegates failure hook to object' do
         method = Delayed::PerformableMethod.new('object', :size, [])
         expect(method.object).to receive(:failure)
         method.failure
