2e2c3fb8e4e6ab4b9903dc93d602b3bf347b9b8e
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2017-09-05 07:18:18 +0900

    Use `yaml_tag` instead of deprecated `yaml_as`
    `yaml_as` is deprecated since Ruby 1.9.3.
    https://github.com/ruby/ruby/commit/5b5bbdbb35098e8e2dabe7af5d31526a54dc8bbd
    
    And it will be remove in Ruby 2.5.0.
    https://github.com/ruby/ruby/commit/6d77e28763ed17f75edf3b4072701b4dbd7644bb

--- lib/delayed/serialization/active_record.rb
+++ lib/delayed/serialization/active_record.rb
@@ -1,7 +1,7 @@
 if defined?(ActiveRecord)
   module ActiveRecord
     class Base
-      yaml_as 'tag:ruby.yaml.org,2002:ActiveRecord'
+      yaml_tag 'tag:ruby.yaml.org,2002:ActiveRecord'
 
       def self.yaml_new(klass, _tag, val)
         klass.unscoped.find(val['attributes'][klass.primary_key])
